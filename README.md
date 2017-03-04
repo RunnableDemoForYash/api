71af645d7107bbac4df9f22dad44fed06a6d9164
tosih <sohail@runnable.com>
2017-03-03 16:24:51 -0800

    Initial commit of API code.

---
+++ README.md
@@ -0,0 +1,15 @@
+# api
+* A Node.js todo demo API server
+
+## Running locally
+
+```bash
+$ npm install
+$ npm start
+```
+
+## Run Unit tests
+
+```bash
+$ npm run test
+```
---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+
+const bodyParser = require('body-parser')
+
+const app = express()
+const host = process.env.RUNNABLE_CONTAINER_URL || 'localhost'
+const port = 3000
+const mongoHost = process.env.MONGODB_HOST || 'localhost:27017'
+
+mongoose.connect(`mongodb://${mongoHost}/todo`)
+mongoose.connection.on('error', () => {
+  console.log('ERROR: Unable to connect to MongoDB.')
+})
+
+const todos = require('./todos')
+
+function logErrors (err, req, res, next) {
+  console.error(err.stack)
+  next(err)
+}
+
+function errorHandler (err, req, res, next) {
+  res.status(500)
+  res.json({ error: err })
+}
+
+app.listen(port, () => {
+  var hostName = 'http://' + host + ':' + port
+  console.log('Application running at: ' + hostName)
+})
+
+app.use(bodyParser.json())
+app.use(logErrors)
+app.use(errorHandler)
+app.use(cors())
+
+// Routes
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+app.get('/todos', todos.all)
+app.get('/todos/:id', todos.one)
+app.post('/todos', todos.create)
+app.put('/todos/:id', todos.update)
+app.delete('/todos/:id', todos.delete)
